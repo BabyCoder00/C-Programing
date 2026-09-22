073d3f31b18b8882c2cc6dbcbf681554f8b7c3d6
Arya <aryaabhishek200059@gmail.com>
2026-09-22 17:26:46 +0530

    variables

---
+++ variables/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    int age = 25;
+    int year = 2026;
+    int quantity = 1;
+
+    printf("You are %d years old. \n", age);
+    printf("The year is %d. \n", year);
+    printf("You have order %d x quantity.\n", quantity);
+
+    float gpa = 2.5;
+    float price = 19.99;
+    float temp = -10.1;
+
+    printf("your gpa is %f.\n", gpa);
+    printf("the price is $%.2f.\n", price);
+    printf("the temprature is %.1f°F.\n", temp);
+    
+    return 0;
+}
+
+// 0:22

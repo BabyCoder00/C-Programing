53d5bc7b6a88bfe0204e71f41c4deb2810b78e86
Arya <aryaabhishek200059@gmail.com>
2026-09-23 16:46:06 +0530

    arithmetic operation

---
+++ arithemetic_operators/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(){
+
+    // int x = 2;
+    // int y = 3;
+    // int z = 0;
+
+    // z =x + y;
+    // z =x - y;
+    // z =x * y;
+    // z =x / y;
+
+
+    // int x = 2;
+    // float y = 3;
+    // float z = 0;
+
+    // z =x/y;
+
+    // printf("%f\n", z);
+
+    int x = 10;
+    int y = 3;
+    int z = 0;
+
+    // z = x % y;
+
+    // printf("%d\n", z);
+    
+    // x++;
+    // x--;
+    
+    // x+=2;
+    // x-=3;
+    // x*=3;
+    x/=2;
+
+
+    printf("%d\n", x);
+
+    return 0;
+}
